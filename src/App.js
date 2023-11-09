f7cdf038a55f07cc03598acfe19c30b43a02c7c2
Shoko Kimera <shokokimera@gmail.com>
2023-11-09 14:27:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Particulars from './Particulars'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Particulars />
     </div>
   );
 }
---
+++ src/Particulars.jsx
@@ -0,0 +1,10 @@
+function Particulars () {
+    return (
+        <>
+            <h1>Shoko S. Kimera</h1>
+            <p>I code and stuff</p>
+        </>
+    )
+}
+
+export default Particulars;
