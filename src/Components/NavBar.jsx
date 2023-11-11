59446cb1f56a1961c200f46999426bebd1325665
Shoko Kimera <shokokimera@gmail.com>
2023-11-11 22:09:13 +0300

    update navbar links

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,11 +6,11 @@ function NavBar() {
                 <li><strong>5HOKO</strong></li>
             </ul>
             <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Skills</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#" className="contrast">About</a></li>
+                <li><a href="#" className="contrast">Skills</a></li>
+                <li><a href="#" className="contrast">Projects</a></li>
+                <li><a href="#" className="contrast">Testimonials</a></li>
+                <li><a href="#" className="contrast">Contact</a></li>
             </ul>
             <ul>
 
