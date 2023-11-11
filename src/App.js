1170e6fe718f2529f23c39bda4474bc9c833cf7a
Shoko Kimera <shokokimera@gmail.com>
2023-11-11 17:32:19 +0300

    import pico css and add container class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import About from './Components/About';
-import './App.css';
+// import './App.css';
 import Contact from './Components/Contact';
 import NavBar from './Components/NavBar';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
 import Testimonials from './Components/Testimonials';
+import '@picocss/pico'
 
 function App() {
   return (
     <>
-      <NavBar />
-      <About />
-      <Skills />
-      <Projects />
-      <Testimonials />
-      <Contact />
+      <main className="container">
+        <NavBar />
+        <About />
+        <Skills />
+        <Projects />
+        <Testimonials />
+        <Contact />
+      </main>
     </>
 
   );
