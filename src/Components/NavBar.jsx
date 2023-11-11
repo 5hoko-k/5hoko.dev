e4bc949940221fa89ec4c03bfbc8f67d72f4a49e
Shoko Kimera <shokokimera@gmail.com>
2023-11-11 17:52:27 +0300

    update nav links and add particulars

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,9 +6,14 @@ function NavBar() {
                 <li><strong>5HOKO</strong></li>
             </ul>
             <ul>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <ul>
+
             </ul>
         </nav>
         </>
--- src/Components/Particulars.jsx
+++ src/Components/Particulars.jsx
@@ -1,8 +1,12 @@
 function Particulars () {
     return (
         <>
+        <hgroup>
             <h1>Shoko S. Kimera</h1>
-            <p>I code and stuff</p>
+            <h2>I code and stuff</h2>
+        </hgroup>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quam in eius eaque earum laboriosam quis ab assumenda, atque libero necessitatibus sunt mollitia sapiente qui maxime nihil ut sit error.</p>
         </>
     )
 }
