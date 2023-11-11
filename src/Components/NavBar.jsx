2719a3514e096f6387247e81ce3b51bfb3e33a9a
Shoko Kimera <shokokimera@gmail.com>
2023-11-11 17:35:00 +0300

    add nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,16 @@
 function NavBar() {
     return (
         <>
-        
+        <nav>
+            <ul>
+                <li><strong>5HOKO</strong></li>
+            </ul>
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </nav>
         </>
     )
 }
