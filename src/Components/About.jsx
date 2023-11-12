3f439c3b640dbb5487bc0e7ab6b5c07f49acb4dc
Shoko Kimera <shokokimera@gmail.com>
2023-11-12 12:39:16 +0300

    add links to other sections in about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,9 +3,17 @@ import Particulars from './Particulars'
 function About() {
     return (
         <>
-            <div className="App">
+            <section>
                 <Particulars />
-            </div>
+                <div className='container-fluid'>
+                    <nav>
+                        <ul>
+                            <li><a href='#' role='button' className='secondary'>Work with me</a></li>
+                            <li><a href='#' role='button' className='contrast'>See my past work</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
         </>
     )
 }
