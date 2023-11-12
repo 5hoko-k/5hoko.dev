fad4a07f4e9c40176fed7a2c37180fe23fc5aad3
Shoko Kimera <shokokimera@gmail.com>
2023-11-12 22:31:07 +0300

    update skills section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,39 @@
+import { skills } from "../Data";
+
 function Skills() {
     return (
         <>
-        
+        <section className="">
+            <h2>Skills</h2>
+            <div className="container-fluid">
+                <hgroup>
+                    <h3>Languages</h3>
+                    <div className="grid">
+                        {skills.languages.map((lan) => {
+                        return <h5 key={lan}>{lan}</h5>
+                        })}
+                    </div>
+                </hgroup>
+                <hgroup>
+                    <h3>Frameworks</h3>
+                    <div className="grid">
+                        {skills.frameworks.map((framework) => {
+                        return <h5 key={framework}>{framework}</h5>
+                        })}
+                    </div>
+                </hgroup>
+                <hgroup>
+                    <h3>Databases</h3>
+                    <div className="grid">
+                        {skills.databases.map((database) => {
+                        return <h5 key={database}>{database}</h5>
+                        })}
+                    </div>
+                </hgroup>
+            </div>
+
+
+        </section>
         </>
     )
 }
