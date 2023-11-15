0efa4ee24f18e3850aa4fa35d2f18a50f90042be
Shoko Kimera <shokokimera@gmail.com>
2023-11-15 19:56:49 +0300

    add grid to render all projects listed in projects data

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,27 @@
+import { projects } from "../Data";
+
 function Projects() {
     return (
         <>
-        
+        <section>
+            
+            <h1>Projects</h1>
+            
+            <div className="grid">
+                {projects.map(({ title, description, link, image }) => {
+                    return (
+                        <article>
+                            Project Image
+                            <footer>
+                                <h5>{title}</h5>
+                                <p>{description}</p>
+                            </footer>
+                        </article>
+                    )
+                })}
+            </div>
+
+        </section>
         </>
     )
 }
