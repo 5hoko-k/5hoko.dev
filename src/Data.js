b3056349c44601d7d9236515b4844c863c652d20
Shoko Kimera <shokokimera@gmail.com>
2023-11-16 22:32:58 +0300

    upfate projects

--- src/Data.js
+++ src/Data.js
@@ -9,11 +9,11 @@ export const projects = [
     image: '',
     description: 'A library record of Anime experienced by 5hoko himself',
     link: 'https://5-aa.vercel.app/' },
-    { title: 'Somn',
+    { title: 'Somn1',
     image: '',
     description: 'About that somn',
     link: 'https://www.google.com/' },
-    { title: 'Somn',
+    { title: 'Somn2',
     image: '',
     description: 'About that somn',
     link: 'https://www.google.com/' }
