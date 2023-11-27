9fe79f620e0a22deaa6c6386f6b6b5d8f61157f1
Shoko Kimera <shokokimera@gmail.com>
2023-11-27 14:36:34 +0300

    add contact details and form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,41 @@
 function Contact() {
     return (
         <>
+        <h2>Contact</h2>
+        <div className="grid">
+            <div>
+                <iframe></iframe>
+            </div>
+            <div className="grid">
+                <hgroup>
+                    <h3>Adress</h3>
+                    <h5>Some where</h5>
+                </hgroup>
+                <hgroup>
+                    <h3>Email</h3>
+                    <h5>Some email</h5>
+                </hgroup>
+                <hgroup>
+                    <h3>Phone</h3>
+                    <h5>Some number</h5>
+                </hgroup>
+            </div>
+        </div>
+        <form>
+            <label>
+                Name
+            </label>
+            <input />
+            <label>
+                Email
+            </label>
+            <input />
+            <label>
+                Message
+            </label>
+            <input />
+
+        </form>
         
         </>
     )
