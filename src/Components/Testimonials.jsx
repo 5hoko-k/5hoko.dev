1083f08771132ea8b46e5c24a1eb9e3705e25ac1
Shoko Kimera <shokokimera@gmail.com>
2023-11-27 11:07:42 +0300

    render the testimonials in a loop

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,7 +1,19 @@
+import { testimonials } from "../Data";
+
 function Testimonials() {
     return (
         <>
-        
+        {testimonials.map((tMonial) => {
+            return (
+                <blockquote key={tMonial.name}>
+                    <img src='' alt='' />
+                    {tMonial.quote}
+                    <footer>
+                        <cite>{tMonial.name} {tMonial.company}</cite>
+                    </footer>
+                </blockquote>
+            )
+        })}
         </>
     )
 }
