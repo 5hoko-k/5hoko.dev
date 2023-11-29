e88a297257ecdb1139c22651a30b1ef78b519f9f
Shoko Kimera <shokokimera@gmail.com>
2023-11-29 10:31:32 +0300

    wrap form in a card

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,47 +2,53 @@ function Contact() {
     return (
         <>
         <h2>Contact</h2>
-        <div className="grid">
-            <div>
-                <iframe></iframe>
-            </div>
-            <div className="grid">
-                <hgroup>
-                    <h3>Adress</h3>
-                    <h5>Some where</h5>
-                </hgroup>
-                <hgroup>
-                    <h3>Email</h3>
-                    <h5>Some email</h5>
-                </hgroup>
-                <hgroup>
-                    <h3>Phone</h3>
-                    <h5>Some number</h5>
-                </hgroup>
-            </div>
-        </div>
-        <form netlify name='contact-form'> 
+        
+            {/* contact info */}
             <div className="grid">
                 <div>
-                    <label>
-                        Name
-                    </label>
-                    <input type='text' name='name' placeholder="Your name" required/>
+                    <iframe></iframe>
                 </div>
-                <div>
-                    <label>
-                        Email
-                    </label>
-                    <input type='text' name='email' placeholder="Email address" required/>
+                <div className="grid">
+                    <hgroup>
+                        <h3>Adress</h3>
+                        <h5>Some where</h5>
+                    </hgroup>
+                    <hgroup>
+                        <h3>Email</h3>
+                        <h5>Some email</h5>
+                    </hgroup>
+                    <hgroup>
+                        <h3>Phone</h3>
+                        <h5>Some number</h5>
+                    </hgroup>
                 </div>
             </div>
-                <label>
-                    Message
-                </label>
-                <textarea name="text-area"></textarea>
 
-
-        </form>
+            {/* contact form */}
+            <article>
+                <form netlify name='contact-form'> 
+                    <div className="grid">
+                        <div>
+                            <label>
+                                Name
+                            </label>
+                            <input type='text' name='name' placeholder="Your name" required/>
+                        </div>
+                        <div>
+                            <label>
+                                Email
+                            </label>
+                            <input type='text' name='email' placeholder="Email address" required/>
+                            <small>We'll never share your email without concent</small>
+                        </div>
+                    </div>
+                        <label>
+                            Message
+                        </label>
+                        <textarea name="text-area"></textarea>
+                        <button type='submit'>Submit</button>
+                </form>
+            </article>
         
         </>
     )
