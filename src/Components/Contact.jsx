2b6f959195bbf85c791b2eb48939e90c61c111b0
Shoko Kimera <shokokimera@gmail.com>
2023-11-30 15:07:22 +0300

    update the iframe

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -30,7 +30,7 @@ function Contact() {
             {/* contact info */}
             <div className="grid">
                 <div>
-                <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126781.22566732607!2d37.6019603888295!3d-6.780405602899364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185a5dc00cee7437%3A0xf0e8f2f705ae1dd1!2sMorogoro!5e0!3m2!1sen!2stz!4v1701260555907!5m2!1sen!2stz" width="550" height="500" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126781.22566732607!2d37.6019603888295!3d-6.780405602899364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185a5dc00cee7437%3A0xf0e8f2f705ae1dd1!2sMorogoro!5e0!3m2!1sen!2stz!4v1701260555907!5m2!1sen!2stz" width="550" height="500" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className="grid">
                     <hgroup>
