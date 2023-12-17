df2771b7d7bf13669a2e4c6c37ac87bcb1c66061
Shoko Kimera <shokokimera@gmail.com>
2023-12-17 14:13:23 +0300

    add tooltips and paddings

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,41 +7,60 @@ function Skills() {
         <h2>Skills</h2>
         <div className="container-fluid">
           <div>
-            <h3 className="center">Languages</h3>
+            <h3 className="center p-bottom">Languages</h3>
             <div className="grid">
               {skills.languages.map((lan) => {
                 return (
-                    <div className="center">
-                    <img src={lan.logo} alt={lan.name} className="logo-icon-style"/>
+                  <div className="center">
+                    <span data-tooltip={lan.name}>
+                      <img
+                        src={lan.logo}
+                        alt={lan.name}
+                        className="logo-icon-style"
+                      />
+                    </span>
+
                     {/* <p key={lan.name}>{lan.name}</p> */}
-                    </div>
-                )
+                  </div>
+                );
               })}
             </div>
           </div>
           <hgroup>
-            <h3 className="center">Frameworks</h3>
+            <h3 className="center p-bottom">Frameworks</h3>
             <div className="grid">
               {skills.frameworks.map((framework) => {
                 return (
                   <div className="center">
-                  <img src={framework.logo} alt={framework.name} className="logo-icon-style"/>
-                  {/* <h5 key={framework.name}>{framework.name}</h5> */}
+                    <span data-tooltip={framework.name}>
+                      <img
+                        src={framework.logo}
+                        alt={framework.name}
+                        className="logo-icon-style"
+                      />
+                    </span>
+                    {/* <h5 key={framework.name}>{framework.name}</h5> */}
                   </div>
-              )
+                );
               })}
             </div>
           </hgroup>
           <hgroup>
-            <h3 className="center">Databases</h3>
+            <h3 className="center p-bottom">Databases</h3>
             <div className="grid">
               {skills.databases.map((database) => {
                 return (
                   <div className="center">
-                  <img src={database.logo} alt={database.name} className="logo-icon-style"/>
-                  {/* <h5 key={database.name}>{database.name}</h5> */}
+                    <span data-tooltip={database.name}>
+                      <img
+                        src={database.logo}
+                        alt={database.name}
+                        className="logo-icon-style"
+                      />
+                      {/* <h5 key={database.name}>{database.name}</h5> */}
+                    </span>
                   </div>
-              )
+                );
               })}
             </div>
           </hgroup>
