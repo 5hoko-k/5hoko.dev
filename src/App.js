d2f5aef3935ec0bcfe6667690a7af87462f2a654
Shoko Kimera <94675153+5hoko-k@users.noreply.github.com>
2025-03-21 15:27:59 +0300

    make about section more responsive, move it to header tag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import About from './Components/About';
-import './App.css';
-import Contact from './Components/Contact';
-import NavBar from './Components/NavBar';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import Testimonials from './Components/Testimonials';
+import About from "./Components/About";
+import "./App.css";
+import Contact from "./Components/Contact";
+import NavBar from "./Components/NavBar";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Testimonials from "./Components/Testimonials";
 
 function App() {
   return (
     <>
       <NavBar />
-      <main className="container-fluid main-paddings">
+      <header>
         <About />
+      </header>
+      <main className="container-fluid main-paddings">
         <Skills />
         <Projects />
         <Testimonials />
         <Contact />
       </main>
     </>
-
   );
 }
 
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,25 +1,39 @@
-import Particulars from './Particulars'
+import Particulars from "./Particulars";
 
 function About() {
-    return (
-        <>
-            <section id='about' className="section-height">
-                <div className='grid'>
-                    <div className='container-fluid my-auto'>
-                        <Particulars />
-                        <div className='container-fluid'>
-                            <nav>
-                                <ul>
-                                    <li><a href='https://www.google.com/' role='button' className='secondary'>Work with me</a></li>
-                                    <li><a href='#projects' role='button' className='contrast'>See my past work</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                        <div className='my-image'><img src='/5hoko.jpg' alt='Shoko-pfp' /></div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section id="about" className="section-height container-2">
+        <div className="grid">
+          <div className="container-fluid vertical-margin-auto">
+            <Particulars />
+            <div className="container-fluid">
+              <nav>
+                <ul>
+                  <li>
+                    <a
+                      href="https://www.google.com/"
+                      role="button"
+                      className="secondary"
+                    >
+                      Work with me
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#projects" role="button" className="contrast">
+                      See my past work
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div className="my-pfp-image">
+            <img src="/5hoko.jpg" alt="Shoko-pfp" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default About;
